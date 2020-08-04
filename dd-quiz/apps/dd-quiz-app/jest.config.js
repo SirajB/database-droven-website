2dbf44273ffa8bd47a045566ef5556297bf257b8
SirajB <sirajbrepotra@gmail.com>
2020-08-04 13:54:01 +0100

    angular install and jest

--- dd-quiz/apps/dd-quiz-app/jest.config.js
+++ dd-quiz/apps/dd-quiz-app/jest.config.js
@@ -1,21 +1,9 @@
 module.exports = {
-  name: 'dd-quiz-app',
-  preset: '../../jest.config.js',
-  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer',
-      ],
-    },
+  testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
   },
-  coverageDirectory: '../../coverage/apps/dd-quiz-app',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
+  resolver: '@nrwl/jest/plugins/resolver',
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageReporters: ['html'],
 };
